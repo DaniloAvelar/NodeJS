762a688baa4a282cbb489199e3f750a0f2d100ca
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2022-10-10 21:19:32 -0300

    Tipos Constantes

---
+++ TipoNumerico.js
@@ -0,0 +1,8 @@
+const nome = "Danilo"
+const idade = 36
+const altura = 1.86
+const peso = 101
+
+//console.log(altura);
+
+console.log("Nome: "+ nome+"\n" +"Idade: "+idade + "\n" + "Altura: "+altura+"\n"+ "Peso: "+peso);
