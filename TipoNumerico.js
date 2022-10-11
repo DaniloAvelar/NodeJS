c294f1e6e83d0b780e2211177584a76a03e1dfcb
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2022-10-10 21:29:10 -0300

    atualizao aporeadores

--- TipoNumerico.js
+++ TipoNumerico.js
@@ -6,3 +6,21 @@ const peso = 101
 //console.log(altura);
 
 console.log("Nome: "+ nome+"\n" +"Idade: "+idade + "\n" + "Altura: "+altura+"\n"+ "Peso: "+peso);
+
+
+let input = null;
+
+if (input === null)
+{
+    console.log("Não há informações");
+}
+else
+{
+    console.log(input);
+}
+
+const soma = 10 + 10;
+const mult = 10 * 10;
+const operacao = (soma + mult) + 10
+
+console.log(operacao);
