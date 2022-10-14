65453f95018f7d1bfe5a6c838dd39db894c53678
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2022-10-14 19:17:06 -0300

    Template String

---
+++ templateString.js
@@ -0,0 +1,11 @@
+//Template Strings
+
+const nome = "Danilo";
+const idade = 2022 - 1986;
+const nasc = "Ribeirão Preto";
+
+// OBS.: Utilizar o acento Grave para esse exemplo
+
+const apresentacao = `Meu nome é ${nome}, moro em ${nasc}, e atualmente tenho ${idade} anos!`
+
+console.log(apresentacao);
