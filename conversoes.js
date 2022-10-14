e7dbacb17ec35c3df45802de1348dc8818ba1478
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2022-10-14 11:25:58 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// conversão implicita ****************************************
+
+const numero = 456;
+const numStr = "456";
+
+console.log(numero === numStr) // False
+console.log(numero == numStr) // True
+
+//Nesse caso o JavaScript pegou a (const numero) e converteu em String, e depois, conferiu se uma é igual a outra, como ele conveteu, agora uma é == a outra
+
+console.log(numero + numStr) // Após converter o numero em String, o sinal ( + ) nao é uma soma, e sim uma concatenação tendo seu resultado "456456"
+
+// Conversão Explicita *****************************************
+console.log(numero + Number(numStr))
+
