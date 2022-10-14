ae16abb54fb607a55efc3499adeb5ef21101d10f
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2022-10-14 19:04:08 -0300

    Operador ternario

---
+++ opTernario.js
@@ -0,0 +1,14 @@
+// Operador Ternário
+
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+/*if (idadeCliente >= idadeMinima){
+    console.log("Bebida alcoólica liberada")
+} else{
+    console.log("Bebida alcoólica NÃO liberada")
+} */
+
+// TERNÁRIO
+            // condição ================  // true ======================  // false ====================================
+console.log(idadeCliente >= idadeMinima ? "Maioridade, Alcool Liberado" : "Menor de idade, NÃO liberar bebida alcoólica" )
