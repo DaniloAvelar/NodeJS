a100b7249b13f63577ae2c56e22660a57abbc66d
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2022-10-14 11:12:07 -0300

    Booleanos

---
+++ Booleano.js
@@ -0,0 +1,23 @@
+const logado = true
+const ctaPaga = false
+
+// truthy or falsy
+
+// 0 == false
+// 1 == true
+
+console.log (0 == false) // true
+console.log ("" == false) // true
+console.log (1 == true) // true
+
+// null
+// undefined
+
+let qq; // <== undefined
+let qqN = null; // <== NULL
+
+console.log (qq)
+console.log (qqN)
+console.log (typeof qq)
+console.log (typeof qqN)
+
