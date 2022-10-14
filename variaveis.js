3116d962a684a5f0c64433388158b72b2aa91934
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2022-10-14 08:32:23 -0300

    Tipos de Variaveis

---
+++ variaveis.js
@@ -0,0 +1,45 @@
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area;
+
+// ============================= LET ===============================
+
+// let forma = 'retangulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retangulo')
+// {
+//     area = altura * comprimento; 
+// } 
+// else 
+// {
+//     area = altura * comprimento / 2;
+// }
+
+// area = altura * comprimento;
+// console.log(area)
+
+// ========================= CONST =======================
+
+const forma = 'triangulo'
+const altura = 5;
+const comprimento = 7;
+let area;
+if (forma === 'quadrado')
+{
+    area = altura * comprimento; 
+} 
+else 
+{
+    area = altura * comprimento / 2;
+}
+
+console.log(area)
+
+
+
