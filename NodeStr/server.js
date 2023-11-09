2e9185eafa31f214f2d1e70d73d55e82b0d3e965
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2023-11-08 22:25:25 -0300

    Função de tratamento de erros onError

--- NodeStr/server.js
+++ NodeStr/server.js
@@ -5,7 +5,10 @@ const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+//Chamando a função Normalize
+//const port = 3000;
+
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -21,6 +24,50 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+
+//Chamando a função ***onError***
+server.on('error', onError);
+
 console.log('API rodando na porta ' + port);
+
+// Função para normalizar a porta web
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+//Função para gerenciamento de erros
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EAADRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+
+}
+
+
 
 
