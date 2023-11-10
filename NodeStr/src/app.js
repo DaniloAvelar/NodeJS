a63f82aa3eaa0cbb5ab18fd95938b1c183ce4a98
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2023-11-10 10:48:20 -0300

    CRUD BASICO

--- NodeStr/src/app.js
+++ NodeStr/src/app.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
+/* MIDWARES */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+/* Metodo GET - Read */
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API",
@@ -12,6 +19,33 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+/* Metodo Post - Create */
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+/* Metodo Put - Update */
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+/* Metodo Delete */
+const del = router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+
 app.use('/', route);
+app.use('/produtos', create);
+app.use('/produtos', put);
+app.use('/produtos', del);
 
 module.exports = app;
