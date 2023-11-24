be0b48482eb584e716b11c09b6cdf7fdbdc5c11c
Danilo Avelar <danilo.avelar@consorciomagalu.com.br>
2023-11-24 14:52:16 -0300

    Async e Await

--- NodeStr/src/controllers/produtoController.js
+++ NodeStr/src/controllers/produtoController.js
@@ -7,94 +7,93 @@ const repository = require('../repositories/produtoRepository');
 /* Totos os metodos chamam o Repository, que é onde se encontra a real chamada dos produtos */
 
 //Listando todos os produtos
-exports.get = (req, res, next) => {
-    repository
-        .get()
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
+exports.get = async (req, res, next) => {
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar a requisição'
         });
+    }
 }
 
-//Listando produtos por SLUG
-exports.getBySlug = (req, res, next) => {
-    repository
-        .getBySlug(req.params.slug)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
+//Listando por SLUG
+exports.getBySlug = async (req, res, next) => {
+    try {
+        var data = await repository.getBySlug(req.params.slug)
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar a requisição lista por Slug'
         });
+    }
 }
 
-//Listando produtos por ID
-exports.getById = (req, res, next) => {
-    repository
-        .getById(req.params.id)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
+//Listando por ID
+exports.getById = async (req, res, next) => {
+    try {
+        var data = await repository.getById(req.params.id);
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar a requisição lista por Id'
         });
+    }
 }
 
-//Listando produtos por Tag
-exports.getByTag = (req, res, next) => {
-    repository
-        .getByTag(req.params.tag)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            res.status(400).send(e);
+//Listando por Tag
+exports.getByTag = async (req, res, next) => {
+    try {
+        var data = await repository.getByTag(req.params.tag)
+        res.status(200).send(data);
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar a requisição lista por Tag'
         });
+    }
 }
 
 //Cadastrando um produto
-exports.post = (req, res, next) => {
-    repository
-        .create(req.body)
-        .then(x => {
-            res.status(201).send({
-                message: 'Produto codastrado com sucesso!'
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao cadastrar o produto!',
-                data: e
-            })
+exports.post = async (req, res, next) => {
+    try {
+        await repository.create(req.body)
+        res.status(201).send({
+            message: 'Produto cadastrado com sucesso'
         });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao cadastrar o produto!'
+        });
+    }
+
 };
 
 //Atualizando produto
-exports.put = (req, res, next) => {
-    repository
-        .update(req.params.id, req.body)
-        .then(x => {
-            res.status(201).send({
-                message: 'Produto atualizado com sucesso!'
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao atualizar o produto!',
-                data: e
-            })
+exports.put = async (req, res, next) => {
+    try {
+        await repository.update(req.params.id, req.body);
+        res.status(201).send({
+            message: 'Produto atualizado com sucesso',
         });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao atualizar o produto'
+        });
+    }
 };
 
 //Deletando um produto
-exports.delete = (req, res, next) => {
-    repository
-        .delete(req.body.id) // Passando o ID no BODY e não no PARAMS, é mais seguro
-        .then(x => {
-            res.status(201).send({
-                message: 'Produto removido com sucesso!'
-            });
-        }).catch(e => {
-            res.status(400).send({
-                message: 'Falha ao remover o produto!',
-                data: e
-            })
+exports.delete = async (req, res, next) => {
+    try {
+        await repository.delete(req.body.id)
+        res.status(201).send({
+            message: 'Produto removido com sucesso'
         });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao remover o produto!'
+        });
+    }
 };
 
--- NodeStr/src/repositories/produtoRepository.js
+++ NodeStr/src/repositories/produtoRepository.js
@@ -3,45 +3,48 @@
 const mongoose = require('mongoose');
 const Produto = mongoose.model('Produto');
 
-exports.get = () => {
-    return Produto
-        .find({
-            ativo: true
-        }, 'titulo preco slug'); //traz somente esses campos separados por espaço
+exports.get = async () => {
+    const res = await Produto.find({
+        ativo: true
+    }, 'titulo preco slug');
+    return res;
 }
 
-exports.getBySlug = (slug) => {
-    return Produto
+exports.getBySlug = async (slug) => {
+    const res = await Produto
         //.findOne({}) <-- Não traz o resultado como array
         .findOne({
             slug: slug,
             ativo: true
-        }, 'titulo descricao preco slug tags');
+        }, 'titulo descricao preco slug tags')
+    return res;
 }
 
-exports.getById = (id) => {
-    return Produto
+exports.getById = async (id) => {
+    const res = await Produto
         //.findById({}) <-- Traz o resultado, todos os dados, nao precisa passar qual a coluna desejada, filtrando por ID
         .findById(id);
+    return res;
 }
 
-exports.getByTag = (tag) => {
-    return Produto
+exports.getByTag = async (tag) => {
+    const res = await Produto
         //.find({}) <-- Traz o resultado filtrando por Tag (OBS:) getByTag é um recurso do Mongoose
         //Ele faz a busca dentro de um array de string e nos retorna o resultado
-        .find({
+        .findOne({
             tags: tag,
             ativo: true
         }, 'titulo descricao preco slug tags')
+    return res;
 }
 
-exports.create = (data) => {
+exports.create = async (data) => {
     var produto = new Produto(data);
-    return produto.save()
+    await produto.save()
 }
 
-exports.update = (id, data) => {
-    return Produto
+exports.update = async (id, data) => {
+    await Produto
         .findByIdAndUpdate(id, {
             $set: {
                 titulo: data.titulo,
@@ -52,7 +55,7 @@ exports.update = (id, data) => {
         });
 }
 
-exports.delete = (id) => {
-    return Produto
-        .findOneAndDelete(id); 
+exports.delete = async (id) => {
+    await Produto
+        .findOneAndDelete(id);
 }
